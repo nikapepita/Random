680b3aa969b3a22507dadb61b2846e994d24a4ef
nikapepita <annika.ludwig@posteo.de>
2019-11-19 10:56:10 +0100

    .

---
+++ CreatePackage.R
@@ -0,0 +1,17 @@
+###Create your Package
+#Install Rtools
+#####################
+#install.packages(c("devtools","roxygen2"))
+
+library(devtools)
+library(roxygen2)
+
+#Get ready to create a package
+has_devel()
+
+setwd("./")
+getwd()
+
+create("C:/Users/Annika/Documents/Git/Random/")
+
+
